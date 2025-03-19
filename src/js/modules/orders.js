dc26dff9d7372ae3dacfa819ba04c7f41034f850
Задорин Артем <artfan95@gmail.com>
2025-03-19 22:40:04 +0700

    Правки блока Заказы

--- src/js/modules/orders.js
+++ src/js/modules/orders.js
@@ -15,8 +15,16 @@ window.ordersRowDraw = () => {
     const data = [{
       'download_link': '#',
       'calc_date': '07.03.2024',
-      'is active': true,
       'calc status': 'complete',
+      'upper coeff': 10,
+      'lower coeff': 1,
+      'trust': 1,
+      'z': 40,
+      'asd4': '10%',
+      'm': 2,
+      'm1': 5,
+      'm2': 15,
+      'm3': 15,
       'safety stock calc': 'SafetyStockCalc',
       'minimum order quantity percent': '20',
       'minimum order percent': '50',
@@ -25,12 +33,20 @@ window.ordersRowDraw = () => {
     {
       'download_link': '#',
       'calc_date': '17.03.2024',
-      'is active': false,
       'calc status': 'complete',
       'safety stock calc': 'SafetyCalc',
       'minimum order quantity percent': '25',
       'minimum order percent': '10',
       'delivery frequency': '40',
+      'upper coeff': 10,
+      'lower coeff': 1,
+      'trust': 1,
+      'z': 40,
+      'asd4': '10%',
+      'm': 2,
+      'm1': 5,
+      'm2': 15,
+      'm3': 15,
     }];
 
     const arr = Array.from(Array(14).keys());
@@ -40,10 +56,6 @@ window.ordersRowDraw = () => {
         for (const key in data[i]){
           if (key === 'download_link'){
             template += `<td align="center"><a href="${data[i][key]}"><i class="fa fa-cloud-download-alt fa-2x"></i></a></td>`;
-            continue
-          }
-          if (key === 'is active'){
-            template += `<td align="center">${data[i][key] ? '<i class="fa fa-circle-check fa-2x"></i>' : '<i class="fa fa-circle-xmark fa-2x"></i>'}</td>`;
             continue
           }
           template += `<td>${data[i][key]}</td>`;
